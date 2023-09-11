db4eb53017b38de0b3ed7a40220ea74990f585bb
cirbojosha <144733910+cirbojosha@users.noreply.github.com>
2023-09-11 20:12:46 +0000

    Initiate and write Hello World program

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
+/*
+Comment block
+*/
+
 #include <iostream>
+#include <string>
 
 int main() {
+  const std::string MESSAGE = "Hello World Josha Cirbo\n";
+
+  std::cout << MESSAGE;
+  
   return 0;
 }
