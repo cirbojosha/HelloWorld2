91c5aa954b17697b02138331773e655bb7aefa30
cirbojosha <144733910+cirbojosha@users.noreply.github.com>
2023-09-13 03:50:26 +0000

    Reset program to follow original parameters of assignment
    I took out the user input and just added my name to the output message. I think this is what the original assignments was expecting, and adding extra functionality might violate the parameters of the exercise.

--- main.cpp
+++ main.cpp
@@ -4,22 +4,17 @@ Josha Cirbo
 CSC1029 Intro to Secure Coding
 Program name: Hello World 2
 Description:
-  Gets user input and prints 'Hello World user_name' to the console.
+  Prints 'Hello World Josha Cirbo' to the console.
 */
 
 #include <iostream>
 #include <string>
 
 int main() {
-  const std::string MESSAGE = "Hello World ";
-  std::string user_name;
-
-  // Get user input. Use getline incase there are spaces in User name
-  std::cout << "Enter your name: ";
-  getline(std::cin, user_name);
+  const std::string MESSAGE = "Hello World Josha Cirbo";
 
   // Output message to screen
-  std::cout << MESSAGE << user_name << std::endl;
+  std::cout << MESSAGE << std::endl;
   
   return 0;
 }
